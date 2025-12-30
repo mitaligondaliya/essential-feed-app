6c2663d0db02813dee55bce651f15ba3d92c1772
Mitali <gondaliyamitali19@gmail.com>
2025-12-30 15:00:39 +0530

    recommit left changes

--- EssentialFeedApp/EssentialFeedAppTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedApp/EssentialFeedAppTests/RemoteFeedLoaderTests.swift
@@ -5,4 +5,27 @@
 //  Created by Mitali Gondaliya on 30/12/25.
 //
 
-import Foundation
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+    
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        
+        let client = HTTPClient()
+        
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+        
+    }
+}
+
