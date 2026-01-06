030d02b6ba0f364006638932e98e41beb3d2d02a
Mitali <gondaliyamitali19@gmail.com>
2026-01-06 16:43:01 +0530

    Load more than once test corrected tests simplify spy

--- EssentialFeedApp/EssentialFeedAppTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedApp/EssentialFeedAppTests/RemoteFeedLoaderTests.swift
@@ -11,34 +11,45 @@ import EssentialFeedApp
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
+        let (_, client) = makeSUT(url: url)
         
-        let (_, client) = makeSUT()
-        
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
         
     }
     
-    func test_load_requestDataFromURL() {
-        let (sut, client) = makeSUT()
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_load_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
     // Mark: Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
-            requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
